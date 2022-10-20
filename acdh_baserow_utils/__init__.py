099030755d65c20362cf475289b40abfa52b8c67
csae8092 <peter.andorfer@oeaw.ac.at>
2022-10-20 15:22:42 +0200

    formatted with black

--- acdh_baserow_utils/__init__.py
+++ acdh_baserow_utils/__init__.py
@@ -4,10 +4,9 @@ import requests
 from .utils import BaseRowUtilsError
 
 
-class BaseRowClient():
-
+class BaseRowClient:
     def url_fixer(self, url):
-        if url.endswith('/'):
+        if url.endswith("/"):
             return url
         else:
             return f"{url}/"
@@ -23,21 +22,15 @@ class BaseRowClient():
             response = None
             result = None
             x = None
-            response = requests.get(
-                url,
-                headers=self.headers
-            )
+            response = requests.get(url, headers=self.headers)
             result = response.json()
-            next_page = result['next']
-            url = result['next']
-            for x in result['results']:
+            next_page = result["next"]
+            url = result["next"]
+            for x in result["results"]:
                 yield x
 
     def __init__(
-        self,
-        br_table_id=None,
-        br_base_url="https://api.baserow.io/api/",
-        br_token=None
+        self, br_table_id=None, br_base_url="https://api.baserow.io/api/", br_token=None
     ):
         if br_table_id is None:
             raise BaseRowUtilsError(msg="No Table-ID is set")
@@ -45,15 +38,13 @@ class BaseRowClient():
             self.br_table_id = br_table_id
 
         if br_token is None:
-            self.br_token = os.environ.get('BASEROW_TOKEN', 'NOT_SET')
+            self.br_token = os.environ.get("BASEROW_TOKEN", "NOT_SET")
         else:
             self.br_token = br_token
-        if self.br_token is None or self.br_token == 'NOT_SET':
+        if self.br_token is None or self.br_token == "NOT_SET":
             raise BaseRowUtilsError
 
-        self.headers = {
-            "Authorization": f"Token {self.br_token}"
-        }
+        self.headers = {"Authorization": f"Token {self.br_token}"}
 
         self.br_base_url = self.url_fixer(br_base_url)
         self.br_rows_url = f"{self.br_base_url}database/rows/table/{self.br_table_id}/"
--- setup.py
+++ setup.py
@@ -4,14 +4,11 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
+with open("README.md") as readme_file:
     readme = readme_file.read()
 
 
-requirements = [
-    'pandas>=1.4.0,<2',
-    'requests>=2.20.0,<3'
-]
+requirements = ["pandas>=1.4.0,<2", "requests>=2.20.0,<3"]
 
 setup_requirements = []
 
@@ -19,29 +16,25 @@ test_requirements = []
 
 setup(
     author="Peter Andorfer",
-    author_email='peter.andorfer@oeaw.ac.at',
-    python_requires='>=3.8',
+    author_email="peter.andorfer@oeaw.ac.at",
+    python_requires=">=3.8",
     classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
     ],
     description="Utility functions to work with Baserow",
     install_requires=requirements,
     license="MIT license",
     long_description=readme,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     include_package_data=True,
-    package_data={
-        '': ['fixtures/*.*'],
-        'acdh_collatex_utils': ['xslt/*.xsl']
-    },
-    name='acdh_baserow_pyutils',
-    packages=find_packages(include=['acdh_baserow_pyutils', 'acdh_baserow_pyutils.*']),
+    name="acdh_baserow_pyutils",
+    packages=find_packages(include=["acdh_baserow_pyutils", "acdh_baserow_pyutils.*"]),
     setup_requires=setup_requirements,
-    test_suite='tests',
+    test_suite="tests",
     tests_require=test_requirements,
-    url='https://github.com/acdh-oeaw/acdh-baserow-pyutils',
-    version='0.0.1',
+    url="https://github.com/acdh-oeaw/acdh-baserow-pyutils",
+    version="0.0.1",
     zip_safe=False,
 )

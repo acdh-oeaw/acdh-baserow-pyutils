2e2ee760aebbffe6126a8e1a73925697d4a0e180
csae8092 <peter.andorfer@oeaw.ac.at>
2022-10-21 11:05:53 +0200

    now #2 should be fixed

--- tests/test_baserow_client.py
+++ tests/test_baserow_client.py
@@ -5,6 +5,7 @@ from acdh_baserow_utils import BaseRowClient
 
 
 TABLE_ID = "100948"
+DATABASE_ID = "41426"
 BASEROW_USER = os.environ.get('BASEROW_USER')
 BASEROW_PW = os.environ.get('BASEROW_PW')
 BASEROW_TOKEN = os.environ.get('BASEROW_TOKEN')
@@ -23,3 +24,11 @@ class TestBaseRowClient(unittest.TestCase):
     def test_001_iterate_rows(self):
         hansi = [x for x in BR_CLIENT.yield_rows(TABLE_ID)]
         self.assertTrue('id' in hansi[0].keys())
+
+    def test_002_list_tables(self):
+        tables = BR_CLIENT.list_tables(DATABASE_ID)
+        self.assertEqual(len(tables), 3)
+
+    def test_003_fix_url(self):
+        url_fixer = BR_CLIENT.url_fixer('hansi')
+        self.assertEqual(url_fixer[-1], '/')

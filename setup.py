f1eb99b29d0fa054c78056c0ac2a58ca04c58eee
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-11-14 14:18:34 +0100

    added enrich_data

--- setup.py
+++ setup.py
@@ -37,6 +37,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/acdh-oeaw/acdh-baserow-pyutils",
-    version="0.1.2",
+    version="0.2.0",
     zip_safe=False,
 )

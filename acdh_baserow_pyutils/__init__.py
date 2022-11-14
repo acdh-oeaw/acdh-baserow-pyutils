2964b11b681735c7b560544fa57da0d950b56dcd
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2022-11-14 14:32:01 +0100

    removed enrich_data

--- acdh_baserow_pyutils/__init__.py
+++ acdh_baserow_pyutils/__init__.py
@@ -2,9 +2,6 @@ import json
 import os
 import requests
 
-from acdh_id_reconciler import gnd_to_wikidata, geonames_to_wikidata
-from AcdhArcheAssets.uri_norm_rules import get_normalized_uri
-
 
 class BaseRowClient:
     def get_jwt_token(self):
@@ -58,57 +55,6 @@ class BaseRowClient:
                     json.dump(data, f, ensure_ascii=False)
             file_names.append(f_name)
         return file_names
-
-    def enrich_data(self, br_table_id, uri, field_name_input, field_name_update):
-        table = [x for x in self.yield_rows(br_table_id=br_table_id)]
-        br_rows_url = f"{self.br_base_url}database/rows/table/{br_table_id}/"
-        v_wd = 0
-        v_geo = 0
-        for x in table:
-            update = {}
-            if uri == "gnd":
-                if (len(x[field_name_input["gnd"]]) > 0):
-                    norm_id = get_normalized_uri(x[field_name_input["gnd"]])
-                    print(norm_id)
-                    try:
-                        wd = gnd_to_wikidata(norm_id)
-                        wd = wd["wikidata"]
-                        v_wd += 1
-                        print(f"gnd id matched with wikidata: {wd}")
-                    except Exception as err:
-                        wd = "N/A"
-                        print(err)
-                        print(f"no match for {norm_id} found.")
-                    update[field_name_update["wikidata"]] = wd
-            if uri == "geonames":
-                if (len(x[field_name_input["geonames"]]) > 0):
-                    norm_id = get_normalized_uri(x[field_name_input["geonames"]])
-                    print(norm_id)
-                    try:
-                        geo = geonames_to_wikidata(norm_id)
-                        gnd = geo["gnd"]
-                        wd = geo["wikidata"]
-                        v_geo += 1
-                        print(f"geonames id matched with gnd: {gnd} and wikidata: {wd}")
-                    except Exception as err:
-                        wd = "N/A"
-                        gnd = "N/A"
-                        print(err)
-                        print(f"no match for {norm_id} found.")
-                    update[field_name_update["gnd"]] = f"https://d-nb.info/gnd/{gnd}"
-                    update[field_name_update["wikidata"]] = wd
-            row_id = x["id"]
-            url = f"{br_rows_url}{row_id}/?user_field_names=true"
-            print(url)
-            try:
-                requests.patch(
-                    url,
-                    headers=self.headers,
-                    json=update
-                )
-            except Exception as err:
-                print(err)
-        print(f"{v_wd} wikidata uri and {v_geo} geonames uri of {len(table)} table rows matched")
 
     def __init__(
         self,

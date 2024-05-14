e9ae420e7452ee1b246458902c0936c0e58620d8
csae8092 <peter.andorfer@oeaw.ac.at>
2024-05-14 08:56:53 +0200

    version bump [skip ci]

--- setup.py
+++ setup.py
@@ -38,6 +38,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/acdh-oeaw/acdh-baserow-pyutils",
-    version="0.5",
+    version="0.9",
     zip_safe=False,
 )
